dc6641b57c0a9124701af84e99657378bc7b7bad
JOAN MANUEL DIAZ SALAZAR <jdiazs4@miumg.edu.gt>
2019-05-29 23:32:39 -0600

    Creacion del proyecto final de la clase de Ingenieria de Software implementando SSO

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet-master/AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/CalculadoraController.cs
@@ -0,0 +1,109 @@
+﻿using AppModelv2_WebApp_OpenIDConnect_DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculadoraModel model, string command)
+        {
+            if (command == "Suma")
+            {
+                model.Result = Suma(model.A, model.B).ToString();
+                Console.WriteLine(model.Result);
+            }
+            if (command == "Resta")
+            {
+                model.Result = Resta(model.A, model.B).ToString();
+            }
+            if (command == "Multiplicacion")
+            {
+                model.Result = Multi(model.A, model.B).ToString();
+            }
+            if (command == "Division")
+            {
+                model.Result = Divi(model.A, model.B).ToString();
+            }
+            return View(model);
+        }
+
+        public double Suma(double a, double b)
+        {
+            double resultado = 0;
+            var url = "https://iscalculadora.appspot.com/sumar?a=" + a + "&b=" + b;
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = reader.ReadToEnd();
+                resultado = Convert.ToInt32(result);
+            }
+
+            return resultado;
+        }
+
+        public double Resta(double a, double b)
+        {
+            double resultado = 0;
+            var url = "https://iscalculadora.appspot.com/restar?a=" + a + "&b=" + b;
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = reader.ReadToEnd();
+                resultado = Convert.ToInt32(result);
+            }
+
+            return resultado;
+        }
+
+        public double Multi(double a, double b)
+        {
+            double resultado = 0;
+            var url = "https://iscalculadora.appspot.com/multiplicar?a=" + a + "&b=" + b;
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = reader.ReadToEnd();
+                resultado = Convert.ToInt32(result);
+            }
+
+            return resultado;
+        }
+
+        public double Divi(double a, double b)
+        {
+            double resultado = 0;
+            var url = "https://iscalculadora.appspot.com/dividir?a=" + a + "&b=" + b;
+            var webrequest = (HttpWebRequest)System.Net.WebRequest.Create(url);
+
+            using (var response = webrequest.GetResponse())
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var result = reader.ReadToEnd();
+                resultado = Convert.ToDouble(result);
+            }
+
+            return resultado;
+        }
+
+    }
+}
---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet-master/AppModelv2-WebApp-OpenIDConnect-DotNet/Models/CalculadoraModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Models
+{
+    public class CalculadoraModel
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public string Result { get; set; }
+    }
+}
